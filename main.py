896c5b4ce63c8609bd747d3090eb548f3a786d6a
Adam Duston <adam.duston@ge.com>
2016-08-02 16:51:16 -0700

    Fix the config.json.
    Properly implement reading in the config values, and detecting which redis services exist in the environment.

--- main.py
+++ main.py
@@ -12,11 +12,43 @@ import json
 from flask import Flask, jsonify, abort
 from flask import __version__ as flask_version
 
-CONFIG_FILE = "./config/config.json"
-REDIS_INSTANCES = ""
 app = Flask(__name__)
 
-test_values = {'food': 'cheese', 'drink': 'coffee', 'animal': 'cajpybara', '1': 'two'}
+test_values = {'food': 'cheese', 'drink': 'coffee', 'animal': 'capybara', '1': 'two'}
+
+# Config file should be provided to specify runtime values if not running in CF.
+# If no config is set the app should log an error and fail.
+# Checks for VCAP_APPLICATION as an indicator of a CloudFoundry environment, and loads
+# the config file only if CF is not detected.
+CONFIG_FILE = "./config/config.json"
+config = ""
+if not os.getenv("VCAP_APPLICATION"):
+    try:
+        app.logger.info("CF environment not found. Trying config file.")
+        with open(CONFIG_FILE, 'r') as config_file:
+            config = json.load(config_file)
+
+    except IOError:
+        app.logger.error("Could not open config file {0} - Quitting.".format(CONFIG_FILE))
+        sys.exit(1)
+
+    except ValueError:
+        app.logger.error("Could not load config file. Does not contain valid JSON data.")
+        sys.exit(1)
+
+# Look for redis instances in the CF
+redis_instances = list()
+vcap_services = os.getenv("VCAP_SERVICES")
+if vcap_services:
+    services = json.loads(vcap_services)
+    for k in services.keys():
+        for i in services[k]:
+            if 'redis' in i['tags']:
+                redis_instances.append(i)
+elif config:
+    redis_instances = config['redis']
+else:
+    redis_instances = list()
 
 
 @app.route('/')
@@ -24,14 +56,16 @@ def main():
     """ The 'main' route / will just report some basic environment information for now. """
     python_version = sys.version
     newline = "<br/>"
-    redis_instances = "".join(newline, REDIS_INSTANCES)
+    redis_info = ""
+    for i in redis_instances:
+        redis_info += "{0}:{1}{2}".format(i['credentials']['host'], i['credentials']['port'], newline)
 
     return "python-cf-redis-api started successfully." \
            "{2}{2}Python: {0}{2}" \
            "Flask: {1}" \
            "{2}{2}" \
            "Redis instances:{2}" \
-           "{3}".format(python_version, flask_version, newline, redis_instances)
+           "{3}".format(python_version, flask_version, newline, redis_info)
 
 
 @app.route('/api/keys/<string:key_value>', methods=['GET'])
@@ -42,7 +76,7 @@ def get_value(key_value):
     except KeyError:
         abort(404)
 
-    return jsonify({key_value: value})
+    return jsonify({'value': value})
 
 if __name__ == '__main__':
     """ Setup the app environment and start the Flask application. """
@@ -54,32 +88,9 @@ if __name__ == '__main__':
         # Run the app, listening on all IPs with our chosen port number
         app.run(host='0.0.0.0', port=int(BIND_PORT))
     else:
-        # Config file should be provided to specify runtime values if not running in CF.
-        # If no config is set the app should log an error and fail.
         try:
-            app.logger.info("CF environment not found. Trying config file.")
-            with open(CONFIG_FILE, 'r') as config_file:
-                config = json.load(config_file)
-
-        except IOError:
-            app.logger.error("Could not open config file {0} - Quitting.".format(CONFIG_FILE))
-            sys.exit(1)
-
-        except ValueError:
-            app.logger.error("Could not load config file. Does not contain valid JSON data.")
-            sys.exit(1)
-
-        try:
-            app.run(host='0.0.0.0', port=int(9099))
+            app.run(host='0.0.0.0', port=int(config['port']))
 
         except ValueError:
             app.logger.error("{0} is not a valid port number. Port must be an integer.".format(config['port']))
             sys.exit(1)
-
-    # Look for redis instances in the CF
-    vcap_services = os.getenv("VCAP_SERVICES")
-
-    if vcap_services:
-        services = json.loads(vcap_services)
-
-
